cee9384dc00bf1cac98444281e9200e91f99ef75
Pedram <aguiardpedram@gmail.com>
2024-10-18 11:13:30 +0200

    Test nécessaire pour affichage

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,7 @@
-// import axios from 'axios';
-
-// const API_URL = 'http://localhost:3000';
-
-// export default {
-//     register(user) {
-//         return axios.post(`${API_URL}/register`, user);
-//     },
-//     login(credentials) {
-//         return axios.post(`${API_URL}/login`, credentials);
-//     },
-//     getMatches() {
-//         return axios.get(`${API_URL}/matches`);
-//     },
-//     createMatch(match) {
-//         return axios.post(`${API_URL}/matches`, match);
-//     }
-// };
-
-
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.VUE_APP_API_URL,
 });
 
 export default {
--- src/components/MatchesComponent.vue
+++ src/components/MatchesComponent.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="matches-container">
-    <h1>Upcoming Handball Matches</h1>
-    <ul v-if="matches.length">
-      <li v-for="match in matches" :key="match.id" class="match-item">
-        {{ match.team1 }} vs {{ match.team2 }} - {{ formatDate(match.match_date) }}
-      </li>
-    </ul>
-    <p v-else>No matches available</p>
+    <h1>Handball - Match à venir</h1>
+    <DataView :value="products">
+      <template #list="slotProps">
+        <section class="flex flex-row gap-5" v-for="match in slotProps.items" :key="match.id">
+          <span class="font-bold text-2xl">{{ match.team1 }}</span>
+          <span class="font-bold text-6xl">{{ match.score }}</span>
+          <span class="font-bold text-2xl">{{ match.team2 }}</span>
+        </section>
+      </template>
+      <template #empty>
+        <div class="flex justify-content-center h-15rem align-items-center">
+          <span class="font-medium text-6xl">Pas de match à venir !</span>
+        </div>
+      </template>
+    </DataView>
   </div>
 </template>
 
@@ -15,21 +23,15 @@
 import api from '../api/api';
 
 export default {
-    data() {
-        return {
-            matches: []
-        };
-    },
-    created() {
-        api.getMatches().then(response => {
-            this.matches = response.data;
-        });
-    },
-    methods: {
-    formatDate(dateString) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-      return new Date(dateString).toLocaleDateString(undefined, options);
-    },
+  data() {
+    return {
+      matches: []
+    };
+  },
+  created() {
+    api.getMatches().then(response => {
+      this.matches = response.data;
+    });
   },
 };
 
