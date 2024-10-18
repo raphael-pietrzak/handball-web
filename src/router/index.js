c3fe72cfdf507999387f72ea3c28477ee96c0add
trp-789 <jean.perrut@ynov.com>
2024-10-18 10:10:46 +0200

    rebase: prfile sur vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,23 @@ import LoginPage from '@/views/LoginPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import MatchesView from '@/views/MatchesView.vue'
 import HomeView from '@/views/HomeView.vue'
-import ProfileView from '@/views/ProfileView.vue'
 import ShopView from '@/views/ShopView.vue'
 import SignUpView from '@/views/SignUpView.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
 
 const routes = [
   { path: '/', redirect: "/home" },
   { path: '/login', name: 'login',component: LoginPage },
   { path: '/signup', name: 'signup', component: SignUpView },
+
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfilePage
+  },
   { path: '/home', name: 'navbar', component: HomeView },
   { path: '/matches', name: 'matches', component: MatchesView },
-  { path: '/profile', name: 'profile', component: ProfileView },
+  { path: '/profile', name: 'profile', component: ProfilePage },
   { path: '/shop', name: 'shop', component: ShopView },
 
 
